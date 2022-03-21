fd4340a3735e75fde054572b6a5cde9053ea73a5
Eric Greene <eric@t4d.io>
2022-03-21 15:28:38 -0400

    add settings

---
+++ python_demos/dates_demo.py
@@ -0,0 +1,14 @@
+""" dates demo """
+
+# from datetime import date, datetime, timedelta
+import time
+
+def run_demo() -> None:
+    """ run demo """
+
+    current_time = time.time()
+
+    print(current_time)
+
+if __name__ == "__main__":
+    run_demo()
