492bad4fc8e8a680167cc5a9f2e7ba5dbdcaf84d
Eric Greene <eric@t4d.io>
2022-03-21 19:10:39 -0400

    add flask demo

--- python_demos/rates_demo/__main__.py
+++ python_demos/rates_demo/__main__.py
@@ -1,19 +1,21 @@
 """ rates demo main """
 
-from datetime import date
-#from rates_demo.dates_demo import run_demo
+# from datetime import date
+# from rates_demo.dates_demo import run_demo
 # from .dates_demo import run_demo
-
-from .business_days import business_days
+# from .business_days import business_days
+from rates_demo.rates_api import start_rates_api
 
 
 
 if __name__ == "__main__":
     #run_demo()
 
-    the_start_date = date(2020, 12, 15)
-    the_end_date = date(2021, 1, 14)
+    # the_start_date = date(2020, 12, 15)
+    # the_end_date = date(2021, 1, 14)
 
-    for business_day in business_days(the_start_date, the_end_date):
-        print(business_day)
+    # for business_day in business_days(the_start_date, the_end_date):
+    #     print(business_day)
+
+    start_rates_api()
 
---
+++ python_demos/rates_demo/rates_api.py
@@ -0,0 +1,15 @@
+""" rates api """
+
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route("/")
+def home() -> Response:
+    """ home """
+    return "<h1>Hello, World!</h1>"
+
+def start_rates_api() -> None:
+    """ start rates api """
+
+    app.run()
