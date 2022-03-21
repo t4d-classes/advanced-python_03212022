723580f77461e9a2a0249c524ce339ac1d5006d7
Eric Greene <eric@t4d.io>
2022-03-21 18:44:13 -0400

    add solution to exercise 1

--- python_demos/rates_demo/__main__.py
+++ python_demos/rates_demo/__main__.py
@@ -1,9 +1,19 @@
 """ rates demo main """
 
-from rates_demo.dates_demo import run_demo
+from datetime import date
+#from rates_demo.dates_demo import run_demo
 # from .dates_demo import run_demo
+
+from .business_days import business_days
 
 
 
 if __name__ == "__main__":
-    run_demo()
+    #run_demo()
+
+    the_start_date = date(2020, 12, 15)
+    the_end_date = date(2021, 1, 14)
+
+    for business_day in business_days(the_start_date, the_end_date):
+        print(business_day)
+
