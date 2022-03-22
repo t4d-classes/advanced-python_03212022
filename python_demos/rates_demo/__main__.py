282ac8ca01cbd183a99c28a303e19e04b746609f
Eric Greene <eric@t4d.io>
2022-03-22 19:32:54 -0400

    add get rates thread object demo

--- python_demos/rates_demo/__main__.py
+++ python_demos/rates_demo/__main__.py
@@ -15,8 +15,10 @@ if __name__ == "__main__":
         print("start get rates")
         start_date = date(2021, 1, 1)
         end_date = start_date + timedelta(days=19)
+        currency_symbols = ['USD', 'CAD', 'GBP']
 
-        rates = get_rates_threaded(start_date, end_date)
+        rates = get_rates_threaded(
+            start_date, end_date, "INR", currency_symbols)
 
         print("\n".join(rates))
         print(rates)
--- python_demos/rates_demo/get_rates.py
+++ python_demos/rates_demo/get_rates.py
@@ -1,6 +1,8 @@
 """ get rates """
 
 from concurrent.futures import ThreadPoolExecutor
+import threading
+
 from datetime import date
 
 import requests
@@ -45,16 +47,53 @@ def get_rate_task(business_day: date) -> str:
     return str(response.status_code)
 
 
-def get_rates_threaded(start_date: date, end_date: date) -> list[str]:
+def get_rates_threadpool(start_date: date, end_date: date) -> list[str]:
     """ get rates threaded """
 
     rate_responses: list[str] = []
 
-    with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers=12) as executor:
         rate_responses = list(executor.map(
             get_rate_task,
             [ business_day
               for business_day in business_days(start_date, end_date) ]
         ))
+
+    return rate_responses
+
+
+def get_rate_task2(
+    business_day: date, base_currency: str,
+    currency_symbols: list[str], responses: list[str]) -> None:
+    """ get rate task2 """
+
+    rate_url = "".join([
+        "http://127.0.0.1:5050/api/",
+        str(business_day),
+        "?base=" + base_currency + "&symbols=",
+        ",".join(currency_symbols)
+    ])
+
+    response = requests.get(rate_url)
+    responses.append(response.text)
+
+
+def get_rates_threaded(
+    start_date: date, end_date: date,
+    base_currency: str, currency_symbols: list[str]) -> list[str]:
+    """ get rates """
+
+    rate_responses: list[str] = []
+    threads: list[threading.Thread] = []
+
+    for business_day in business_days(start_date, end_date):
+        a_thread = threading.Thread(
+            target=get_rate_task2,
+            args=(business_day,base_currency,currency_symbols,rate_responses))
+        a_thread.start()
+        threads.append(a_thread)
+
+    for a_thread in threads:
+        a_thread.join()
 
     return rate_responses
